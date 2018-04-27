33675a387f27599b0dd5ed9889fb9266311361da
Mateusz Smagieł <mateusz.smagiel@gmail.com>
2018-04-27 22:13:58 +0200

    init commit

---
+++ src/main/java/pl/denzeloff/currencyapp/CurrencyAppApplication.java
@@ -0,0 +1,12 @@
+package pl.denzeloff.currencyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyAppApplication.class, args);
+	}
+}
