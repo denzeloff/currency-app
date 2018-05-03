c28fb8f01a1c2cb0a031f46c20df9aee86182341
Mateusz Smagieł <mateusz.smagiel@gmail.com>
2018-05-03 22:37:39 +0200

    Added handling exception for wrong date range.

--- src/main/java/pl/denzeloff/currencyapp/UI/CurrencyAppUI.java
+++ src/main/java/pl/denzeloff/currencyapp/UI/CurrencyAppUI.java
@@ -5,10 +5,15 @@ import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpClientErrorException;
 import pl.denzeloff.currencyapp.model.CurrencyCode;
 import pl.denzeloff.currencyapp.service.CurrencyAppService;
 
 import java.time.LocalDate;
+
+
+import static java.time.temporal.ChronoUnit.DAYS;
 
 @SpringUI
 @Theme("valo")
@@ -52,10 +57,39 @@ public class CurrencyAppUI extends UI {
         mainLayout.setSpacing(true);
         setContent(mainLayout);
 
+        startDate.setRangeStart(LocalDate.of(2001, 12, 31));
+        startDate.setRangeEnd(LocalDate.now());
+        endDate.setRangeStart(LocalDate.of(2001, 12, 31));
+        endDate.setRangeEnd(LocalDate.now());
+
+        startDate.addValueChangeListener(e -> {
+            startDate.setRangeEnd(endDate.getValue());
+            endDate.setRangeStart(startDate.getValue());
+            long daysBetween = DAYS.between(startDate.getValue(), endDate.getValue());
+            if (daysBetween > 367) {
+                Notification.show("Days range must be smaller then 367 days", Notification.Type.WARNING_MESSAGE);
+            }
+
+        });
+        endDate.addValueChangeListener(e -> {
+            endDate.setRangeStart(startDate.getValue());
+            startDate.setRangeEnd(endDate.getValue());
+            long daysBetween = DAYS.between(startDate.getValue(), endDate.getValue());
+            if (daysBetween > 367) {
+                Notification.show("Days range must be smaller then 367 days", Notification.Type.WARNING_MESSAGE);
+            }
+        });
 
         getMethodButton.addClickListener(e -> {
-            averageLabel.setValue(currencyAppService.averageCurrencyCost(currencyAppService.listOfBuyingCostCurrency(currencyAppService.getCurrencyJsonObj(getListOfCurrencyCode(), getStartDate(), getEndDate()))).toString());
-            averageLabel.setVisible(true);
+            try {
+                averageLabel.setValue(currencyAppService.averageCurrencyCost(currencyAppService.listOfBuyingCostCurrency(currencyAppService.getCurrencyJsonObj(getListOfCurrencyCode(), getStartDate(), getEndDate()))).toString());
+                averageLabel.setVisible(true);
+            } catch (HttpClientErrorException e1) {
+                if (e1.getStatusCode().equals(HttpStatus.NOT_FOUND))
+                    Notification.show("Sorry, but we don't have date for selected range", Notification.Type.WARNING_MESSAGE);
+                else if (e1.getStatusCode().equals(HttpStatus.BAD_REQUEST))
+                    Notification.show("Pleas choose correct range", Notification.Type.WARNING_MESSAGE);
+            }
         });
     }
 
--- src/main/java/pl/denzeloff/currencyapp/service/CurrencyAppService.java
+++ src/main/java/pl/denzeloff/currencyapp/service/CurrencyAppService.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Service
 public class CurrencyAppService {
 
-    public CurrencyJsonModel getCurrencyJsonObj(String code,String startDate,String endDate) {
+    public CurrencyJsonModel getCurrencyJsonObj(String code, String startDate, String endDate) {
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForObject("http://api.nbp.pl/api/exchangerates/rates/C/" + code + "/" + startDate + "/" + endDate + "/?format=json", CurrencyJsonModel.class);
     }
@@ -36,7 +36,7 @@ public class CurrencyAppService {
         for (int i = 0; i < listOfBuyingCostCurrency.size(); i++) {
             sum = sum.add(listOfBuyingCostCurrency.get(i));
         }
-        average = sum.divide(length,4,RoundingMode.CEILING);
+        average = sum.divide(length, 4, RoundingMode.CEILING);
         return average;
     }
 
